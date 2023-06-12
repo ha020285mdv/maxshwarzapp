8a8bdc783aac38a6edcdb291cfab58869a1998c4
Dmytro Mykytiuk <dmytro.mykytiuk@tietoevry.com>
2023-06-12 15:42:59 +0200

    done until directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './components/server/server.component';
+import { ServersComponent } from './components/servers/servers.component';
+import { WarningComponent } from './components/assets/warning/warning.component';
+import { SuccessComponent } from './components/assets/success/success.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/server/server.component.ts
@@ -0,0 +1,92 @@
+import { Component } from '@angular/core';
+
+
+interface IServer {
+  name: string | null;
+  inUse: number;
+  withHtml(): string;
+  progressClass(): string;
+}
+
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent {
+
+  servers_list: IServer[] = [];
+  allowAddingServer: boolean = true;
+  serverCreationStatus: string = 'No server was created';
+  servername: string = '';
+  message: string = '';
+
+  constructor() {
+    this.createServerComponent("Apple");
+    this.createServerComponent("AWS");
+    this.createServerComponent("IBM");
+    this.createServerComponent("Samsung");
+  }
+
+  createServerComponent(serverName: string) {
+    this.servers_list.push({
+      name: serverName,
+      inUse: Math.random(),
+      withHtml(): string {
+        return `style="width: ${this.inUse * 100}%"`
+      },
+      progressClass(): string {
+        if (this.inUse < 0.25) 
+          {return "info"}
+        else if (this.inUse < 0.65) 
+          {return "success"}
+        else if (this.inUse < 0.90) 
+          {return "warning"}
+        else 
+          {return "danger"}
+       
+      },
+    })
+  }
+
+  enableAddServer(interval: number) {
+    setTimeout(() => {
+      this.allowAddingServer = true;
+    }, interval);
+  }
+
+  onUpdateServerName() {
+    if (this.servers_list.find(
+      element => element.name == this.servername
+      ) == undefined
+      ) {
+        this.createServerComponent(this.servername);
+        this.servername = '';
+        this.allowAddingServer = false;
+        this.enableAddServer(2000);
+    }
+    else {
+      this.message = 'Server already exists'
+    }
+
+  }
+
+  onDeleteServer(server: IServer) {
+    this.servers_list.forEach((value,index)=>{
+      if(value.name==server.name) this.servers_list.splice(index,1);
+    });
+  }
+
+  onSortServersAsc() {
+    this.servers_list.sort((a, b) => (a.inUse < b.inUse ? -1 : 1));
+  }
+
+  onSortServersDesc() {
+    this.servers_list.sort((a, b) => (a.inUse > b.inUse ? -1 : 1));
+  }
+
+
+}
+
+
